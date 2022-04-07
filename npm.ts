8dfa02cac88be60bd58f970153ae7cbe5e298a02
Charles Lowell <cowboyd@frontside.com>
2022-04-07 10:47:01 +0300

    release 0.1.0

--- npm.ts
+++ npm.ts
@@ -17,7 +17,7 @@ await build({
   package: {
     // package.json properties
     name: "@frontside/continuation",
-    version: "0.0.2",
+    version: "0.1.0",
     description: "Delimited continuations for JavaScript",
     license: "MIT",
     repository: {
