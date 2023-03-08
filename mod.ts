608f2de04aa321813c3ac8b8d6663e205158a12a
Eric Bower <me@erock.io>
2023-03-08 16:01:20 -0500

    wip

--- mod.ts
+++ mod.ts
@@ -8,40 +8,69 @@ export interface Continuation<T = any, R = any> {
   (value: T): R;
 }
 
-export function* reset<T>(block: () => Computation): Computation<T> {
-  return yield { type: "reset", block };
+function* _continue<T>(
+  block: (resolve: Continuation<T>, reject: Continuation<Error>) => Computation,
+  value?: T,
+): Computation<T> {
+  return yield { type: "continue", block, value };
+}
+
+function* _throw(block: () => Computation, value: Error): Computation<Error> {
+  return yield { type: "throw", block, value };
+}
+
+export function* reset(block: () => Computation): Computation {
+  return yield* _continue(function* (resolve) {
+    resolve(undefined);
+    yield* block();
+  });
 }
 
 export function* shift<T>(
   block: (resolve: Continuation<T>, reject: Continuation<Error>) => Computation,
 ): Computation<T> {
-  return yield { type: "shift", block };
+  return yield* _continue<T>(block);
 }
 
-export function evaluate<T>(block: () => Computation, getNext = $next()): T {
+export function evaluate<T>(block: () => Computation): T {
   let stack = [block()];
+  return reduce(stack);
+}
+
+function reduce<T>(initStack: Computation[]): T {
+  let stack = [...initStack];
   let value: any;
-  for (let current = stack.pop(); current; current = stack.pop()) {
+  let reducing = true;
+  let current = stack.pop();
+  let getNext = $next();
+
+  while (current) {
     let prog = current[Symbol.iterator]();
     try {
       let next = getNext(prog);
-      getNext = (iter) => iter.next(value);
       if (next.done) {
         value = next.value;
       } else {
-        let cont = ({ [Symbol.iterator]: () => prog });
+        let cont = {
+          [Symbol.iterator]: () => prog,
+        };
         let control = next.value;
-        if (control.type === "reset") {
-          stack.push(cont);
-          stack.push(control.block());
-        } else {
-          let resolve: Continuation = oneshot((value) =>
-            evaluate(() => cont, $next(value))
-          );
-          let reject: Continuation<Error> = oneshot((error) =>
-            evaluate(() => cont, $throw(error))
-          );
+        if (control.type === "continue") {
+          getNext = (iter) => iter.next(control.value);
+          let resolve = oneshot((v: T) => {
+            stack.push(_continue(() => cont, v));
+            if (reducing) return;
+            return reduce(stack);
+          });
+          let reject = oneshot((v: Error) => {
+            stack.push(_throw(() => cont, v));
+            if (reducing) return;
+            return reduce(stack);
+          });
           stack.push(control.block(resolve, reject));
+        } else if (control.type === "throw") {
+          getNext = $throw(control.value);
+          stack.push(cont);
         }
       }
     } catch (error) {
@@ -50,8 +79,12 @@ export function evaluate<T>(block: () => Computation, getNext = $next()): T {
       } else {
         getNext = $throw(error);
       }
+    } finally {
+      current = stack.pop();
     }
   }
+
+  reducing = false;
   return value;
 }
 
@@ -69,23 +102,20 @@ function oneshot<T, R>(fn: Continuation<T, R>): Continuation<T, R> {
 }
 
 const $next = (value?: any) => (i: Iterator<Control>) => i.next(value);
-const $throw = (error: Error) =>
-  (i: Iterator<Control>) => {
-    if (i.throw) {
-      return i.throw(error);
-    } else {
-      throw error;
-    }
-  };
+const $throw = (error: Error) => (i: Iterator<Control>) => {
+  if (i.throw) {
+    return i.throw(error);
+  } else {
+    throw error;
+  }
+};
 
 export type K<T = any, R = any> = Continuation<T, R>;
 
-export type Control =
+export type Control<V = any> =
   | {
-    type: "shift";
-    block(resolve: Continuation, reject: Continuation<Error>): Computation;
+    type: "continue";
+    block(resolve?: Continuation, reject?: Continuation<Error>): Computation;
+    value: V | undefined;
   }
-  | {
-    type: "reset";
-    block(): Computation;
-  };
+  | { type: "throw"; block(): Computation; value: Error };
